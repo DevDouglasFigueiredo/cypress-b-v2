75b3c94d380a41f22540c5edf79b64e519ff108c
DevDouglasFigueiredo <devdouglasfigueiredo@gmail.com>
2024-09-12 16:58:47 -0300

    elementos radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -13,13 +13,13 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     it('preenche os campos obrigatórios e envia o formulário', function () {
         cy.fillMandatoryFields('Douglas', 'Figueiredo', 'devdouglas@gmail.com.br', 'aaaaaaaaaaaaaaaaaaa');
 
-        cy.contains('Enviar').click();
+        cy.get('button[type="submit"]').click();
         cy.get('.success').should('be.visible');
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
         cy.fillMandatoryFields('Douglas', 'Figueiredo', 'devdouglas.gmail.com.br', 'aaaaaaaaaaaaaaaaaaa');
-        cy.contains('Enviar').click();
+        cy.get('button[type="submit"]').click();
         cy.get('.error').should('be.visible');
     })
 
@@ -27,14 +27,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
        
         cy.fillMandatoryFields('Douglas','Figueiredo','devdouglas@gmail.com.br','aaaaaaaaaaaaaaaaaaa');
         cy.get('#phone').type('abcdef').should('have.value', '')
-        cy.contains('Enviar').click();
+        cy.get('button[type="submit"]').click();
         cy.get('.success').should('be.visible');
     })
 
-    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
         cy.fillMandatoryFields('Douglas','Figueiredo','devdouglas@gmail.com.br','aaaaaaaaaaaaaaaaaaa');
         cy.get('#phone-checkbox').click();
-        cy.contains('Enviar').click();
+        cy.get('button[type="submit"]').click();
         cy.get('.error').should('be.visible');
     })
 
@@ -48,8 +48,29 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.', function () {
-        cy.contains('Enviar').click();
-
+        cy.get('button[type="submit"]').click();
         cy.get('.error').should('be.visible');
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', function () {
+        cy.fillMandatoryFields('Douglas','Figueiredo','devdouglas@gmail.com.br','aaaaaaaaaaaaaaaaaaa');
+        cy.get('#product').select('YouTube').should('have.value', 'youtube')
+        cy.get('button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', function () {
+        cy.fillMandatoryFields('Douglas','Figueiredo','devdouglas@gmail.com.br','aaaaaaaaaaaaaaaaaaa');
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+        cy.get('button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
+    })
+
+    it.only('marca o tipo de atendimento "Feedback"', function () {
+        cy.fillMandatoryFields('Douglas','Figueiredo','devdouglas@gmail.com.br','aaaaaaaaaaaaaaaaaaa');
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+        cy.get('input[type="radio"][value="feedback"]').check().should('have.value', 'feedback')
+        cy.get('button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
     })
 });
